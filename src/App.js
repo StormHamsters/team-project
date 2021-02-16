5d8b6ae61cf99b9227f94d747bf4babc97732f05
StormHamsters <78374978+StormHamsters@users.noreply.github.com>
2021-02-15 19:04:23 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          this is a test.
+          this is a test
         </p>
         <a
           className="App-link"
