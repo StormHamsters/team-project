fee14536f877b24f598f28e7595b297d529475f4
StormHamsters <78374978+StormHamsters@users.noreply.github.com>
2021-04-21 21:27:33 -0500

    Add files via upload
    Updated version of game now includes map/world generation and spawning of items to be picked up.

--- src/Game.js
+++ src/Game.js
@@ -1,20 +1,35 @@
 import React, { useRef, useEffect, useState } from "react";
 import Input from "./Input";
+import Player from "./Player";
+import Spawn from "./Spawn";
+import World from "./World";
 
 //after every input, the DOM re-renders using the functions here
 
 const Game = ({ width, height, tilesize }) => {
   const canvasRef = useRef(); // create a reference to the canvas in the DOM
-  const [player, setPlayer] = useState({ x: 64, y: 128 }); //creating the state manager for player using useState hook, setting the initial player postion
+  const [player, setPlayer] = useState(new Player(1, 2, tilesize)); //creating the state manager for player using useState hook, setting the initial player postion
+  const [world, setWorld] = useState(new World(width, height, tilesize));
   let input = new Input(); // creating an instance of the Input class so we can handle input for the player
 
   const handleInput = (action, data) => {
     //self-explanitory, but...this function decides what we will do with our input
-    let newPlayer = { ...player }; //spread operator pulls a copy of 'player' and creates a new instance called NewPlayer
-    newPlayer.x += data.x * tilesize; //grabbing the input 'data', or what vector (direction) we are told to move in and translating that to our new x-coordinate
-    newPlayer.y += data.y * tilesize; //same as above but for the y-coordinate
-    setPlayer(newPlayer); // and then updating the state of Player with setPlayer
+    let newWorld = new World(); //spread operator pulls a copy of 'world' and creates a new instance called newWorld
+    Object.assign(newWorld, world); //copy properties from newPlayer to our new Player object class
+    newWorld.movePlayer(data.x, data.y);
+    setWorld(newWorld); // and then updating the state of Player with setPlayer
   };
+
+  useEffect(() => {
+    console.log("Create Map!");
+    let newWorld = new World();
+    Object.assign(newWorld, world);
+    newWorld.createCellularMap();
+    newWorld.moveToSpace(world.player);
+    let spawn = new Spawn(newWorld);
+    spawn.spawnLoot(10);
+    setWorld(newWorld);
+  }, []);
 
   useEffect(() => {
     //creating a useEffect hook that updates when the DOM is changed, this one runs our input functions on our input class instance, then turns around and runs our functions to reverse those functions to await the next input
@@ -29,15 +44,14 @@ const Game = ({ width, height, tilesize }) => {
     //again, useEffect updates the DOM when something changes, and this one is drawing the "map" to the canvas itself
     const context = canvasRef.current.getContext("2d"); // calling a current context of our canvas ref object
     context.clearRect(0, 0, width * tilesize, height * tilesize); // clear canvas before drawing
-    context.fillStyle = "#000"; //black
-    context.fillRect(player.x, player.y, 16, 16); //actual drawing of player coordinates with 16 by 16 tile size with black square
+    world.draw(context);
   });
   return (
     <canvas
       ref={canvasRef}
       width={width * tilesize}
       height={height * tilesize}
-      style={{ border: "1px solid black" }}
+      style={{ border: "1px solid black", background: "dimgrey" }}
     ></canvas>
     // we are creating a canvas container element to represent our game world. initialized with the values discussed before.
     // also, we connect it to our instance of the game canvas by 'referencing' our canvasRef function created up top.
---
+++ src/Player.js
@@ -0,0 +1,32 @@
+import Entity from "./Entity";
+class Player extends Entity {
+  inventory = [];
+  attributes = {
+    name: "Player",
+    ascii: "@",
+    health: 10,
+  };
+  move(dx, dy) {
+    this.x += dx;
+    this.y += dy;
+  }
+
+  add(item) {
+    this.inventory.push(item);
+  }
+
+  draw(context) {
+    context.fillStyle = "#f00";
+    context.textBaseline = "hanging";
+    context.font = "16 px Helvetica";
+    context.fillText("@", this.x * this.size, this.y * this.size);
+  }
+
+  copyPlayer() {
+    let newPlayer = new Player();
+    Object.assign(newPlayer, this);
+    return newPlayer;
+  }
+}
+
+export default Player;
